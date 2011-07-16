0920506af7f270b23033e394e9e2849b4b796789
Chris Ammerman <cammerman@gmail.com>
2011-07-15 23:34:31 -0500

    Added Home module and view.

---
+++ DeckedOut/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Jessica;
+using Jessica.ViewEngine.Razor;
+
+namespace DeckedOut
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Jess.Initialise();
+            Jess.ViewEngines.Add(new RazorViewEngine());
+        }
+    }
+}
---
+++ DeckedOut/Modules/Home.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Jessica;
+
+namespace DeckedOut.Modules
+{
+    public class Home : JessModule
+    {
+        public Home()
+        {
+            Get("/", p => View("Home"));
+        }
+    }
+}
